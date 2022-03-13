92999371c01208c0df1f9f8d37c98807b44236a6
MingshanHe <hemingshan_1999@163.com>
2022-03-14 01:32:04 +0800

    add device_hardware

---
+++ device_hardware/include/device_driver/device_driver.h
@@ -0,0 +1,29 @@
+#ifndef DEVICE_DRIVER_H
+#define DEVICE_DRIVER_H
+
+#include "device_driver/eci/EciDemo113.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#include <vector>
+
+void init_hardware();
+
+std::vector<double> read_position();
+
+std::vector<double> read_velocity();
+
+std::vector<double> read_torque();
+
+void write_position(std::vector<double> data);
+
+
+#endif
